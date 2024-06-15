3b3cda558b1dacd7ee3295d29a4299d21c332fbc
bolcof <sanctum.crusade.ex@gmail.com>
2024-06-15 11:52:12 +0900

    Physics

--- Assets/Script/PlayerSpawner.cs
+++ Assets/Script/PlayerSpawner.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined {
     public GameObject PlayerPrefab;
+    public GameObject PhysxBall;
 
     public void PlayerJoined(PlayerRef player) {
         if (player == Runner.LocalPlayer) {
             Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+            for(int i=0;i<10;i++) {
+                Runner.Spawn(PhysxBall, new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(3.5f, 4.5f), Random.Range(-0.5f, 0.5f)), Quaternion.identity);
+            }
         }
     }
 }
