7a6b921f872decf700db13db2fbeccfb8ee33fba
bolcof <sanctum.crusade.ex@gmail.com>
2024-06-20 22:06:49 +0900

    でっかくしてみた

--- Assets/Project/Script/GameObject/Paddle/PaddleContoroller.cs
+++ Assets/Project/Script/GameObject/Paddle/PaddleContoroller.cs
@@ -1,30 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PaddleController : MonoBehaviour {
-    public Camera mainCamera;  // メインカメラの参照
-    public float paddleHeight;  // パドルの高さ
-    public Vector2 xRange = new Vector2(-6.25f, 6.25f);  // X軸の移動範囲
-    public Vector2 zRange = new Vector2(-12.5f, -0.6f);  // Z軸の移動範囲
+public class PuckController : MonoBehaviour {
+    private Rigidbody rb;
 
-    private void Awake() {
-        paddleHeight = transform.localScale.y;
+    public float moveSpeed = 5f; // オブジェクトの動きの速さ
+    public Vector2 xRange = new Vector2(-62.5f, 62.5f);  // X軸の移動範囲
+    public Vector2 zRange = new Vector2(-125f, -6f);  // Z軸の移動範囲
+    public Vector3 targetPosition;
+
+    private int screenWidth, screenHeight;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
     }
 
     void Update() {
-        // マウスの位置を取得
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit)) {
-            // マウスの位置をXZ平面に投影
-            Vector3 targetPosition = new Vector3(hit.point.x, paddleHeight, hit.point.z);
+        MoveWithMouse();
+    }
 
-            // X軸の範囲内に制限
-            targetPosition.x = Mathf.Clamp(targetPosition.x, xRange.x, xRange.y);
+    void MoveWithMouse() {
+        // マウスのスクリーン座標を取得
+        Vector3 mouseScreenPosition = Input.mousePosition;
 
-            // Z軸の範囲内に制限
-            targetPosition.z = Mathf.Clamp(targetPosition.z, zRange.x, zRange.y);
 
-            // パドルの位置を更新
-            transform.position = targetPosition;
-        }
+        // Rigidbodyの位置をマウスの位置に追従させる
+        targetPosition = new Vector3();
+        targetPosition.x = Remap(mouseScreenPosition.x, 0, screenWidth, xRange.x * 1.2f, xRange.y * 1.2f);
+        targetPosition.x = Mathf.Clamp(targetPosition.x, xRange.x, xRange.y);
+        targetPosition.y = transform.position.y;
+        targetPosition.z = Remap(mouseScreenPosition.y, 0, screenHeight, zRange.x * 1.2f, zRange.y * 1.2f);
+        targetPosition.z = Mathf.Clamp(targetPosition.z, zRange.x, zRange.y);
+
+        // 直接位置を更新するのではなく、スムーズに移動するように力を加える
+        //Vector3 direction = (targetPosition - transform.position).normalized;
+        //rb.MovePosition(direction * moveSpeed);
+
+        transform.position = targetPosition;
+    }
+    public float Remap(float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
