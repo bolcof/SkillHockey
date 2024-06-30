9b75366d4d64369bf134f50b176f4ed19c0431c1
bolcof <sanctum.crusade.ex@gmail.com>
2024-06-30 11:42:06 +0900

    スキルとキャラのデータの形

---
+++ Assets/Project/Script/Class/CharactorData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharactorData {
+    public int id;
+    public string name;
+    public string story;
+    public int specialSkillId;
+    public Sprite selectingImage;
+}
---
+++ Assets/Project/Script/Class/DataHolder.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataHolder : MonoBehaviour {
+    public List<CharactorData> charactors = new List<CharactorData>();
+    public List<SkillData> skillDatas = new List<SkillData>();
+}
