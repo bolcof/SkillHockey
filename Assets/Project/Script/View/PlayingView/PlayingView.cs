d23b121007cca9802e1e90cc3792ff04386a27db
bolcof <sanctum.crusade.ex@gmail.com>
2024-07-23 07:19:17 +0900

    SkillGuide適用

--- Assets/Project/Script/View/PlayingView/PlayingView.cs
+++ Assets/Project/Script/View/PlayingView/PlayingView.cs
@@ -8,6 +8,7 @@ public class PlayingView : MonoBehaviour {
     public TimeLimit timeLimit;
     public PlayerInfoPanel myPlayerInfo, enemyPlayerInfo;
     public CommandLinePanel commandLinePanel;
+    [SerializeField] List<SkillGuide> skillGuides = new List<SkillGuide>();
 
     public void Set() {
         myPlayerInfo.Set(GameInfoManager.instance.currentSelectCharactorId);
@@ -15,8 +16,11 @@ public class PlayingView : MonoBehaviour {
         //TODO: tmp
         int enemyCharaId = 1 - GameInfoManager.instance.currentSelectCharactorId;
         GameInfoManager.instance.enemyCharactorId = enemyCharaId;
-        
+
         enemyPlayerInfo.Set(enemyCharaId);
+        for (int i = 0; i < skillGuides.Count; i++) {
+            skillGuides[i].Set(DataHolder.instance.charactors[GameInfoManager.instance.currentSelectCharactorId].SkillIds[i]);
+        }
     }
 
     public void setTime(float currentTime) {
