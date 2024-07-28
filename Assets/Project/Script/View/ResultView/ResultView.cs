1db9ec3a48c76f45c9a1957fb9a42e8676ff7201
bolcof <sanctum.crusade.ex@gmail.com>
2024-07-28 12:40:14 +0900

    位置組み込み 機能繋ぎ直し

--- Assets/Project/Script/View/ResultView/ResultView.cs
+++ Assets/Project/Script/View/ResultView/ResultView.cs
@@ -7,27 +7,28 @@ using TMPro;
 
 public class ResultView : MonoBehaviour {
     [SerializeField] private Image background;
-    [SerializeField] private List<GameObject> winnerImageObject, loserImageObject;
-    [SerializeField] private TextMeshProUGUI operatorLabel, winnerCharaLabel, resultLabel;
+    [SerializeField] private List<GameObject> winnerImageObjects, loserImageObjects;
+    [SerializeField] private TextMeshProUGUI winnerOperatorLabel, loserOperatorLabel;
+    //[SerializeField] private TextMeshProUGUI winnerCharaLabel, resultLabel;
 
     public void Set(int playerCharaId, int enemyCharaId, bool hasPlayerWin) {
-        foreach(var wio in winnerImageObject) {
+        foreach (var wio in winnerImageObjects) {
             wio.SetActive(false);
         }
-        foreach(var lio in loserImageObject) {
+        foreach (var lio in loserImageObjects) {
             lio.SetActive(false);
         }
 
         if (hasPlayerWin) {
-            operatorLabel.text = "Player";
-            winnerImageObject[playerCharaId].SetActive(true);
-            loserImageObject[enemyCharaId].SetActive(true);
-            winnerCharaLabel.text = DataHolder.instance.charactors[playerCharaId].name;
+            winnerOperatorLabel.text = "Player";
+            loserOperatorLabel.text = "CPU";
+            winnerImageObjects[playerCharaId].SetActive(true);
+            loserImageObjects[enemyCharaId].SetActive(true);
         } else {
-            operatorLabel.text = "CPU";
-            winnerImageObject[enemyCharaId].SetActive(true);
-            loserImageObject[playerCharaId].SetActive(true);
-            winnerCharaLabel.text = DataHolder.instance.charactors[enemyCharaId].name;
+            winnerOperatorLabel.text = "CPU";
+            loserOperatorLabel.text = "Player";
+            winnerImageObjects[enemyCharaId].SetActive(true);
+            loserImageObjects[playerCharaId].SetActive(true);
         }
 
     }
