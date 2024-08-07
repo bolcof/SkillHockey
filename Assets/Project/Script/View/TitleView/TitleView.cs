0c2443ef7e090dfbe7cf27f31768a5ee9f1e4a66
bolcof <sanctum.crusade.ex@gmail.com>
2024-08-07 20:47:51 +0900

    座標系わかんね〜

--- Assets/Project/Script/View/TitleView/TitleView.cs
+++ Assets/Project/Script/View/TitleView/TitleView.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class TitleView : MonoBehaviour {
 
+    [SerializeField] private List<float> highlightPositions = new List<float>();
+    [SerializeField] private Image highlightBar;
+
+    public void Start() {
+        Debug.Log(highlightBar.GetComponent<RectTransform>().position);
+        highlightBar.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+    }
+
     public void PushStart() {
+        highlightBar.DOFade(0.0f, 0.2f);
+        TransitCharactorSelectView();
+    }
+
+    private void TransitCharactorSelectView() {
         ViewManager.instance.charactorSelectView.gameObject.SetActive(true);
         ViewManager.instance.charactorSelectView.Set(GameInfoManager.GameMode.CPU);
         gameObject.SetActive(false);
 
         GameInfoManager.instance.currentGameMode = GameInfoManager.GameMode.CPU;
+    }
+
+    public void HoverButton(int id) {
+        highlightBar.DOFade(1.0f, 0.225f);
+        highlightBar.GetComponent<RectTransform>().DOMoveY(highlightPositions[id], 0.125f);
     }
 }
