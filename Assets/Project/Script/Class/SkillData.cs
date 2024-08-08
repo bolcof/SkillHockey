a1e011263a92f6d9896365f6bca4ce8d50247e04
bolcof <sanctum.crusade.ex@gmail.com>
2024-08-08 15:58:03 +0900

    SkillInfo修正
    mainViewの見え方直さなきゃ

--- Assets/Project/Script/Class/SkillData.cs
+++ Assets/Project/Script/Class/SkillData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SkillData {
     public string name;
     public List<int> command = new List<int>(); // up:0 down:1 left:2 right:3
-    public string discription;
+    [TextArea(3, 10)] public string discription;
     public Sprite icon;
     public int level;
 }
