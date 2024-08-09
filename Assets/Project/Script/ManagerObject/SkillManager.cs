897d5db6fb364bc258b595ad62683166b3ec1812
bolcof <sanctum.crusade.ex@gmail.com>
2024-08-09 09:26:18 +0900

    制限時間の概念

--- Assets/Project/Script/ManagerObject/SkillManager.cs
+++ Assets/Project/Script/ManagerObject/SkillManager.cs
@@ -4,33 +4,77 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour {
 
+    private List<bool> isActing = new List<bool>();
+    private List<float> remainTime = new List<float>();
+
+    public void Start() {
+        Reset();
+    }
+
+    public void Reset() {
+        isActing.Clear();
+        remainTime.Clear();
+        for (int i = 0; i < DataHolder.instance.skillDatas.Count; i++) {
+            isActing.Add(false);
+            remainTime.Add(0.0f);
+        }
+    }
+
     public void CastSkill(int skillId) {
+        switch (skillId) {
+            case 0:
+                CastSkill_000();
+                break;
+            case 1:
+                CastSkill_001();
+                break;
+            case 2:
+                CastSkill_002();
+                break;
+            case 3:
+                CastSkill_003();
+                break;
+            case 4:
+                CastSkill_004();
+                break;
+            case 5:
+                CastSkill_005();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void FixedUpdate() {
 
     }
 
-    //double
+    //Giant Grip
     public void CastSkill_000() {
-
+        remainTime[0] = 15.0f;
     }
 
-    //speed Up
+    //Blitz Drive
     public void CastSkill_001() {
 
     }
 
-    //big Mallet
+    //Assault Fuly
     public void CastSkill_002() {
 
     }
 
+    //Twin Shade
     public void CastSkill_003() {
 
     }
 
+    //Shadow Veil
     public void CastSkill_004() {
 
     }
 
+    //Mystery Pack
     public void CastSkill_005() {
 
     }
